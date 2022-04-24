7051926c02d4cdf6a1ea231d5d889530412fa80d
Jeancar Sanchez <jeancar.anime.514@gmail.com>
2022-04-24 10:45:45 -0400

    resultado_final

--- script.js
+++ script.js
@@ -8,15 +8,16 @@ botonNav.addEventListener('click', () => {
 const carousel = [...document.querySelectorAll('.carousel-item')];
 const carouselIndicators = [...document.querySelectorAll('.carousel-indicators button')];
 
-carouselIndicators.forEach((item, index) => {
-    item.addEventListener('click', () => {
-        carouselIndicators.forEach(item => item.classList.remove('active'));
-        carouselIndicators[index].classList.add('active');
-        setTimeout(() => {
+const recorrerCarrousel = (index) => {
+    carouselIndicators.forEach(item => item.classList.remove('active'));
+    carouselIndicators[index].classList.add('active');
+    setTimeout(() => {
         carousel.forEach(item => item.classList.remove('active'));
         carousel[index].classList.add('active');
-        }, 100);
-        
+    }, 100);
+};
+carouselIndicators.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        recorrerCarrousel(index);
     });
 });
-
